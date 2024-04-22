386895405847b295be58dea2790511c9ffe4874c
Damarjati <damarjati2409@gmail.com>
2024-04-22 14:07:40 +0700

    commit react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import styled from 'styled-components';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const NavWrapper = styled.nav`
+  position: absolute;
+  right: 0;
+`;
+
+const NavLink = styled.a`
+  color: black;
+  text-align: center;
+  padding: 8px 5px;
+  text-decoration: none;
+`;
+
+const judul = <h2>BOOTCAMP Batch 8 : Experiment with REACTJS</h2>
+ReactDOM.render(judul, document.getElementById("title"));
+
+const element = <h1>This is React</h1>
+ReactDOM.render(element, document.getElementById("root"));
+
+const navigasi = (
+  <NavWrapper>
+    <NavLink href="#Contact">Home</NavLink>
+    <NavLink href="#About">About</NavLink>
+    <NavLink href="#Home">Contact</NavLink>
+  </NavWrapper>
 );
+ReactDOM.render(navigasi, document.getElementById("nav"));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
